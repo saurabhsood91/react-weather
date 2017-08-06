6efb9187ea57912283bc121dd7b33c5e6676fb28
Saurabh Sood <saurabhsood91@gmail.com>
2017-08-06 13:00:35 -0700

    fix error modal

--- app/components/ErrorModal.jsx
+++ app/components/ErrorModal.jsx
@@ -1,4 +1,6 @@
 var React = require('react');
+var ReactDOM = require('react-dom');
+var ReactDOMServer = require('react-dom/server');
 
 var ErrorModal = React.createClass({
     getDefaultProps: function() {
@@ -12,22 +14,31 @@ var ErrorModal = React.createClass({
         message: React.PropTypes.string.isRequired
     },
     render: function() {
-        var {title, message} = this.props;
         return (
-            <div id="error-modal" className="reveal tiny text-center" data-reveal>
-                <h4>{title}</h4>
-                <p>
-                    {message}
-                </p>
-                <p>
-                    <button className="button hollow" data-close="" data-reveal="">
-                        Okay
-                    </button>
-                </p>
+            <div>
             </div>
         );
     },
     componentDidMount: function() {
+        var {title, message} = this.props;
+        var modalMarkup = (<div id="error-modal" className="reveal tiny text-center" data-reveal>
+            <h4>{title}</h4>
+            <p>
+                {message}
+            </p>
+            <p>
+                <button className="button hollow" data-close="" data-reveal="">
+                    Okay
+                </button>
+            </p>
+        </div>);
+
+        // ReactDOMServer.renderToString converts JSX to a string
+        // ReactDOM.findDOMNode finds the DOM node for the component
+        // We use jQuery to append the converted modal string to the DOM
+        var $modal = $(ReactDOMServer.renderToString(modalMarkup));
+        $(ReactDOM.findDOMNode(this)).html($modal);
+
         // Actually open the modal when the component gets mounted on the DOM
         var modal = new Foundation.Reveal($('#error-modal'));
         modal.open();
